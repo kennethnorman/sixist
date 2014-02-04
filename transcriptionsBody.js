124d5cae3bce26107530db01c171cacb20efbbd3
Ken Norman <kn@objectlabsonline.com>
2014-02-04 13:00:41 +0000

    Tidy up, fix name cracking

--- transcriptionsBody.js
+++ transcriptionsBody.js
@@ -1,21 +1,19 @@
 // transcriptionsBody.js
 
+/*
+    @function replaceAll: replace all instances of find with replace in str
+    @param find
+    @param replace
+    @param str
+*/
 function replaceAll(find, replace, str) {
   return str.replace(new RegExp(find, 'g'), replace);
 }
 
 var list = document.getElementById("transcriptionlist");
-/*
-for (var element in transcriptionList) {
-    //alert(transcriptionList[element].title);
 
-    if (list != "undefined") {
-        var formatEntry = transcriptionList[element].artist + " : " + transcriptionList[element].title;
-        list.appendChild(document.createTextNode(formatEntry));
-        list.appendChild(document.createElement("br"));
-    }
-}*/
 
+// Get the song list from the file list.txt
 var xmlhttp;
 
 /* running locally on IE5.5, IE6, IE7 */
@@ -38,18 +36,45 @@ if (!xmlhttp) try { xmlhttp = createRequest(); } catch (e) { xmlhttp = false; }
 
 xmlhttp.open("GET", "list.txt", true);
 
+// Process the song list into something we can output.
 xmlhttp.onreadystatechange = function () {
+    var SongList, SongListLength, i, splitLine, Line, song, songname;
+
     if (xmlhttp.readyState == 4 && (xmlhttp.status == 200 || xmlhttp.status == 0)) {
-        var SongList = xmlhttp.responseText.split(/\n/);
-        var SongListLength = SongList.length;
-        for (var i = 0; i < SongListLength; i++) {
-            var splitLine = SongList[i].split(/Scores\\/);
-            var Line = splitLine[1];
+
+        // Split response into lines.
+        SongList = xmlhttp.responseText.split(/\n/);
+
+        /* 
+        Result list should contain strings in the format
+        
+        Artist\Artist_Title.pdf
+
+        Any marked _private are ignored.
+        Show lines as "Artist : Title"; replace "_" with " " and remove ".pdf".
+
+        */
+        SongListLength = SongList.length;
+        for (i = 0; i < SongListLength; i++) {
+            if (SongList[i].length === 0) {
+                continue;
+            }
+            splitLine = SongList[i].split(/Scores\\/);
+            if (splitLine.length != 2) {
+                continue;
+            }
+            if (splitLine[1].search("_private") != -1) {
+                continue;
+            }
+
+            Line = splitLine[1];
+            // remove \r from line feeds
+            Line = Line.replace("\r", "");
+            Line = replaceAll("_", " ", Line);
             Line = Line.split(/\\/);
-            var song;
             if (Line.length == 2) {
-                var songname= Line[1].replace(Line[0], "");
-                songname= replaceAll("_", " ", songname);
+                songname = Line[1].replace(Line[0], "");
+                songname = songname.replace(".pdf", "");
                 song = Line[0].trim() + " : " + songname.trim();
                 list.appendChild(document.createTextNode(song));
                 list.appendChild(document.createElement("br"));
